b4a1cc27ba81ae1fd8779d9c1390e8cdfec6ed5e
liuye518800 <83738509+liuye518800@users.noreply.github.com>
2021-05-06 09:24:00 +0800

    Add files via upload

---
+++ lytes/src/main/java/com/liu/test/lytes/LytesApplication.java
@@ -0,0 +1,21 @@
+package com.liu.test.lytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class LytesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LytesApplication.class, args);
+    }
+    @PostConstruct
+    public void todosou(){
+        System.out.println("测试输出正确==========================================123");
+        System.out.println("测试输出正确==========================================1345");
+    }
+
+}
